7a872d0a1eba0a3e510f8f97c16ca02a09d4848f
Rohit <rgupta6205@gmail.com>
2024-09-03 18:03:14 +0530

    merge the code with branch mihir

--- hairnic/src/components/CartProduct.jsx
+++ hairnic/src/components/CartProduct.jsx
@@ -1,22 +1,89 @@
-import React, { useState } from "react";
+// import React, { useState } from "react";
+// import productData from "./productData";
+// import { Button } from "react-bootstrap";
+// import { Link } from "react-router-dom";
+// import ProductCard from "./ProductCard";
+
+// const CartProduct = () => {
+//   return (
+//     <div>
+//       <div className="container-fluid py-5">
+//         {/* <div className="container"> */}
+//         <div
+//           className="mx-auto text-center wow fadeIn"
+//           data-wow-delay="0.1s"
+//           style={{ maxWidth: 600 }}
+//         >
+//           <h1 className="text-primary mb-3">
+//             <span className="fw-light text-dark">Our Natural</span>
+//             Products
+//           </h1>
+//           <p className="mb-5">
+//             The broadest definition of natural product is anything that is
+//             produced by life, and includes the likes of biotic materials.
+//           </p>
+//         </div>
+//         <div className="row g-4">
+//           {productData.map(
+//             (
+//               {
+//                 id,
+//                 image,
+//                 name,
+//                 rating,
+//                 reviews,
+//                 regular_price,
+//                 sale_price,
+//               } = product,
+//               index
+//             ) => (
+//               <ProductCard
+//                 key={index}
+//                 id={id}
+//                 image={image}
+//                 name={name}
+//                 rating={rating}
+//                 reviews={reviews}
+//                 regular_price={regular_price}
+//                 sale_price={sale_price}
+//               />
+//             )
+//           {productData.map(({ id, image, name, rating, reviews, regular_price, sale_price } = product, index) =>
+//             <ProductCard
+//               key={index}
+//               id={id}
+//               image={image}
+//               name={name}
+//               rating={rating}
+//               reviews={reviews}
+//               regular_price={regular_price}
+//               sale_price={sale_price}
+//             />
+//           )}
+//         </div>
+//         {/* </div> */}
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default CartProduct;
+
+import React from "react";
 import productData from "./productData";
-import { Button } from "react-bootstrap";
-import { Link } from "react-router-dom";
 import ProductCard from "./ProductCard";
 
 const CartProduct = () => {
   return (
     <div>
       <div className="container-fluid py-5">
-        {/* <div className="container"> */}
         <div
           className="mx-auto text-center wow fadeIn"
           data-wow-delay="0.1s"
           style={{ maxWidth: 600 }}
         >
           <h1 className="text-primary mb-3">
-            <span className="fw-light text-dark">Our Natural</span>
-            Products
+            <span className="fw-light text-dark">Our Natural</span> Products
           </h1>
           <p className="mb-5">
             The broadest definition of natural product is anything that is
@@ -26,15 +93,7 @@ const CartProduct = () => {
         <div className="row g-4">
           {productData.map(
             (
-              {
-                id,
-                image,
-                name,
-                rating,
-                reviews,
-                regular_price,
-                sale_price,
-              } = product,
+              { id, image, name, rating, reviews, regular_price, sale_price },
               index
             ) => (
               <ProductCard
@@ -48,20 +107,8 @@ const CartProduct = () => {
                 sale_price={sale_price}
               />
             )
-          {productData.map(({ id, image, name, rating, reviews, regular_price, sale_price } = product, index) =>
-            <ProductCard
-              key={index}
-              id={id}
-              image={image} 
-              name={name}
-              rating={rating}
-              reviews={reviews}
-              regular_price={regular_price}
-              sale_price={sale_price}
-            />
           )}
         </div>
-        {/* </div> */}
       </div>
     </div>
   );
