0c6e55ca4fa0c154e81a54751aea7c8bfd796e28
sumitranjan245@gmail.com <sumitranjan245@gmail.com>
2024-09-03 12:01:35 +0530

    Update code from team

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ const mongoose = require("mongoose");
 const express = require("express");
 const cors = require("cors");
 const productRoutes = require("./routes/productsRoutes");
-<<<<<<< HEAD
 const vendorRoutes = require("./routes/vendorRoutes");
 
 const reviewRoutes = require("./routes/reviewRoutes");
@@ -12,22 +11,6 @@ require("dotenv").config();
 
 const { MONGODB_URL, PORT } = process.env;
 
-=======
-const addressRoutes = require("./routes/addressRoutes");
-const blogsRoutes = require("./routes/blogsRoutes");
-const adduserRoutes = require("./routes/userRoutes");
-const customersRoutes = require("./routes/customersRoutes");
-const vendorsRoutes = require("./routes/vendorsRoutes");
-const ordersRoutes = require( "./routes/ordersRoutes" );
-const dashboardRoutes = require( "./routes/dashboardRoutes" );
-const refundRoutes = require( "./routes/refundRoutes" );
-
-const app = express();
-require("dotenv").config();
-
-const { MONGODB_URL, PORT } = process.env;
-console.log(MONGODB_URL);
->>>>>>> aa053adb525c8fafa076f3608b83e3e1881ceaef
 const port = PORT || 4000;
 
 mongoose
@@ -41,20 +24,19 @@ app.listen(port, () => {
 app.use(cors());
 app.use(express.json());
 app.use("/api/products", productRoutes);
-<<<<<<< HEAD
+
 app.use("/api/vendors", vendorRoutes);
 app.use("/api/review", reviewRoutes);
 
-=======
 app.use("/api/address", addressRoutes);
 app.use("/api/blogs", blogsRoutes);
 app.use("/api/user", adduserRoutes);
 app.use("/api/customers", customersRoutes);
 app.use("/api/vendors", vendorsRoutes);
-app.use( "/api/orders", ordersRoutes );
-app.use( "/api/dashboard", dashboardRoutes );
-app.use( "/api/refund", refundRoutes );
->>>>>>> aa053adb525c8fafa076f3608b83e3e1881ceaef
+app.use("/api/orders", ordersRoutes);
+app.use("/api/dashboard", dashboardRoutes);
+app.use("/api/refund", refundRoutes);
+
 // app.use('/uploads', uploads)
 
 
